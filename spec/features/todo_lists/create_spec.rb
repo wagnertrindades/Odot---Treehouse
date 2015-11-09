ddea14f01585efc42e8c1b4bb78db94ee35976b9
Wagner Trindade <wtrindades@hotmail.com>
2015-11-09 07:12:49 -0500

    Written test with validate title and implemented validator

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -31,4 +31,22 @@ describe "Creating todo lists" do
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doind today")
 	end
+
+	it "displays an error when the todo list has a title less than 3 characters" do
+		expect(TodoList.count).to  eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "This is what I'm doing today"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to  eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doind today")
+	end
 end
