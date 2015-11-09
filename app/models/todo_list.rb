ce77c43d096fcc02f473ad405bba5d0603d42d9e
Wagner Trindade <wtrindades@hotmail.com>
2015-11-09 07:18:20 -0500

    Implemented validation of presence in description

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ActiveRecord::Base
 	validates :title, presence: true
 	validates :title, length: { minimum: 3 }
+	validates :description, presence: true
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -49,4 +49,22 @@ describe "Creating todo lists" do
 		visit "/todo_lists"
 		expect(page).to_not have_content("This is what I'm doind today")
 	end
+
+	it "displays an error when the todo list has no description" do
+		expect(TodoList.count).to  eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Grocery list"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to  eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Grocery list")
+	end
 end
