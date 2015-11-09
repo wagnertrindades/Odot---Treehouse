90e57766d287eb7c72286f6c6534f08967fa99ba
Wagner Trindade <wtrindades@hotmail.com>
2015-11-09 07:23:01 -0500

    Implemented validation of length in description

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -67,4 +67,22 @@ describe "Creating todo lists" do
 		visit "/todo_lists"
 		expect(page).to_not have_content("Grocery list")
 	end
+
+	it "displays an error when the todo list has a description less than 5 characters" do
+		expect(TodoList.count).to  eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with: "Grocery list"
+		fill_in "Description", with: "test"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to  eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("Grocery list")
+	end
 end
