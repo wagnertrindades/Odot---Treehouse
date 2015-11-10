6d79d2f23aa283b0ed4341a3255068433168075d
Wagner Trindade <wtrindades@hotmail.com>
2015-11-10 06:20:45 -0500

    Added test for validate todo items and associated code

---
+++ spec/features/todo_items/create_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Adding todo items" do
+	let!(:todo_list) { TodoList.create(title: "Grocery List", description: "Groceries") }
+
+	def visit_todo_list(list)
+		visit "/todo_lists"
+		within "#todo_list_#{list.id}" do
+			click_link "List Items"
+		end
+	end
+
+	it "is successfully with valid content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: "Milk"
+		click_button "Save"
+		expect(page).to have_content("Added todo list item.")
+		within "ul.todo_items" do
+			expect(page).to have_content("Milk")
+		end
+	end
+
+	it "displays an error with no content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("Theres was a problem adding that todo list item.")
+		end
+		expect(page).to have_content("Content can't be blank")
+	end
+
+	it "displays an error with content less then 2 characters long" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: "1"
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("Theres was a problem adding that todo list item.")
+		end
+		expect(page).to have_content("Content is too short")
+	end
+end
