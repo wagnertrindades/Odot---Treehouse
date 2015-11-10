561884156b5cd46a352f01394d223340dca6c44a
Wagner Trindade <wtrindades@hotmail.com>
2015-11-10 14:34:12 -0500

    Finalized Course Build a Todo List Application with Rails 4

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe TodoList do
   it { should have_many(:todo_items) }
+
+  describe "#has_complete_items?" do
+  	let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List") }
+
+  	it "returns true with completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo_list.has_completed_items?).to be true 
+  	end
+
+  	it "returns false with no completed todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_completed_items?).to be false
+  	end
+  end
+  describe "#has_complete_items?" do
+  	let(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List") }
+
+  	it "returns true with incompleted todo list items" do
+  		todo_list.todo_items.create(content: "Eggs")
+  		expect(todo_list.has_incompleted_items?).to be true
+  	end
+
+  	it "returns false with no incomplete todo list items" do
+  		todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+  		expect(todo_list.has_incompleted_items?).to be false
+  	end
+  end
 end
